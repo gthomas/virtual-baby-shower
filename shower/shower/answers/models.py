66401b4e412c5f41d8524b4a84a0e83e483d02b4
Grant Thomas <jgrantthomas@gmail.com>
2018-06-20 17:56:56 -0700

    stub out models and views

--- shower/shower/answers/models.py
+++ shower/shower/answers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+    text = models.TextField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User)
+
+class Donation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    note = models.TextField()
+    amount = models.DecimalField()
--- shower/shower/answers/views.py
+++ shower/shower/answers/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def questions(request):
+    pass
+
+def question(request):
+    pass
+
+def answers(request, question_id):
+    pass
+
+def donation(request):
+    pass
+
+def thanks(request):
+    pass
